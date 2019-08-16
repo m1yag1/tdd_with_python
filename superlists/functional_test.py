3ac32e94536d71c609074c8e18e3d66873698b12
m1yag1 <mike.arbelaez@rice.edu>
2019-08-16 14:10:55 -0500

    Redirect after POST, and show all items in template

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -54,7 +54,7 @@ class NewVisitorTest(unittest.TestCase):
         # The page updates again, and now shows both items on her list
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
 
